e2425cb9a6cd138782b1dc542d2ff0361ae5c8f5
LovesAsuna <qq625924077@gmail.com>
2022-11-04 18:36:15 +0800

    :sparkles: feat(structure): stack structure

---
+++ src/stack.rs
@@ -0,0 +1,36 @@
+pub struct List<'a, T> {
+    pub data: T,
+    pub prev: Option<&'a List<'a, T>>,
+}
+
+impl<'a, T> List<'a, T> {
+    pub fn push<U>(
+        prev: Option<&'a List<'a, T>>,
+        data: T,
+        callback: impl FnOnce(&List<'a, T>) -> U,
+    ) -> U {
+        let mut list = List { data, prev };
+        callback(&list)
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a List<'a, T>>,
+}
+
+impl<'a, T> List<'a, T> {
+    pub fn iter(&'a self) -> Iter<'a, T> {
+        Iter { next: Some(self) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.prev.as_ref().map(|prev| &**prev);
+            &node.data
+        })
+    }
+}
